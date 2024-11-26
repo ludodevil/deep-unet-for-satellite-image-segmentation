ac5f2b7e46c358c0406cb9371ea074b9d10d3696
Ludovic de Villelongue <ludovic.devillelongue@hpdia.com>
2024-11-26 18:07:44 +0100

    change test

--- gen_patches.py
+++ gen_patches.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 
-def get_rand_patch(img, mask, sz=160):
+def get_rand_patch(img, mask, sz=80):
     """
     :param img: ndarray with shape (x_sz, y_sz, num_channels)
     :param mask: binary ndarray with shape (x_sz, y_sz, num_classes)
